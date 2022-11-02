a0c5ab6204d2082810d8dcb0cd99e3865da93e18
KKMGeraedts <kkm.geraedts@gmail.com>
2022-11-02 14:18:54 +0100

    bugs fixed

--- momentum_space.py
+++ momentum_space.py
@@ -3,11 +3,36 @@ import numpy as np
 import math
 from data_cleaning import *
 
-def momentum_space_rg(X, step_ratio=0.05, cut_off=0.7, type="covariance"):
+def compute_euclidean_distance(X):
+    """
+    TODO
+    """
+    x_coord = X[:, 0]
+    y_coord = X[:, 1]
+    
+    x_coord_matrix = np.meshgrid(x_coord, x_coord)[1]
+    y_coord_matrix = np.meshgrid(y_coord, y_coord)[1]
+    return np.sqrt((x_coord_matrix - x_coord) ** 2 + (y_coord_matrix - y_coord) ** 2) 
+
+def compute_covariance(X, distance_metric, pca_method):
+    if distance_metric == "covariance":
+        # Compute the covariance matrix or correlation matrix depending on 'pca_method' argument
+        if pca_method == "covariance":
+            return X @ X.T - (np.mean(X, axis=1) * np.mean(X, axis=1).T)
+        elif pca_method == "correlation":
+            return np.corrcoef(X)
+        else:
+            print(f'pca_method argument mussed be either "covariance" or "correlation. Got {pca_method} instead.')
+            return None
+    elif distance_metric == "euclidean":
+        #TODO
+        pass
+
+def momentum_space_rg(X, step_ratio=0.05, cut_off=0.7, pca_method="correlation", distance_metric="covariance"):
     """
     Performs momentum space RG. 
 
-    Input:
+    Parameters:
         X - Data array (numpy)
         cut_off - ratio of modes to be kept after last RG step.
         step_size - percentage of modes to cut at each iteration 
@@ -25,42 +50,23 @@ def momentum_space_rg(X, step_ratio=0.05, cut_off=0.7, type="covariance"):
     n_modes_left_list = []
     X_list = []
 
-    # Compute the covariance matrix or correlation matrix depending on 'type' argument
-    if type == "covariance":
-        covariance = X @ X.T - (np.mean(X, axis=1) * np.mean(X, axis=1).T)
-    elif type == "correlation":
-        covariance = np.corrcoef(X)
-    else:
-        print(f'Type argument mussed be either "covariance" or "correlation. Got {type} instead.')
-        return None
-
-    # eigv, eigh = np.linalg.eig(covariance)
-    # eigv_sorted = np.sort(eigv)[::-1]
-
-    # plt.plot(range(len(eigv_sorted)), eigv_sorted)
-    # plt.title("covariance")
-    # plt.show()
-
-    # SVD
-    u_original, s_origianl, vh = np.linalg.svd(covariance, full_matrices=False)
-
     # Perform RG iterations
     while n_modes_left > n_modes * cut_off:
 
-        # Compute the covariance matrix
-        covariance = X_renormalized @ X_renormalized.T - (np.mean(X, axis=1) * np.mean(X, axis=1).T)
+        # Compute covariance
+        covariance = compute_covariance(X_renormalized, distance_metric, pca_method)
 
         # SVD
-        u, s, vh = np.linalg.svd(covariance, full_matrices=False)
+        u_original, s_original, vh = np.linalg.svd(covariance, full_matrices=False)
 
         # Order eigenvectors and eigenvalues from highest to lowest
-        sort_idx = np.argsort(s)[::-1]
-        s = s[sort_idx]
-        u = u[sort_idx]
+        sort_idx = np.argsort(s_original)
+        s_original = s_original[sort_idx][::-1]
+        u_original = u_original[sort_idx][::-1]
 
         # Save data
         u_list.append(u_original[:n_modes_left-1])
-        s_list.append(s_origianl[:n_modes_left-1])
+        s_list.append(s_original[:n_modes_left-1])
         n_modes_left_list.append(n_modes_left)
         X_list.append(X_renormalized)
 
@@ -81,7 +87,7 @@ def plot_eigenspectrum(s_list, n_modes_left_list):
     Plots the eigenvalue spectrum as a function of their fractional rank for 
     several steps of the coarse-graining. 
 
-    Input:
+    Parameters:
         s_list - list of eigenvalues at several steps of the coarse-graining
         n_modes_left_list - list of the number of modes left at several steps of the coarse-graining
     """
@@ -99,7 +105,7 @@ def plot_normalized_fourth_moment(X_list, n_modes_left_list):
     """
     Compute and plot the normalized fourth moment of all variables across the coarse-graining.
 
-    Input:
+    Parameters:
         X_list - data and reproduced data during coarse-graining
         n_modes_left_lost - number of modes left at acroos the coarse-graining
     """
@@ -117,26 +123,54 @@ def plot_normalized_fourth_moment(X_list, n_modes_left_list):
             )
 
     # Plot Gaussian fixed point
+    print(np.array(n_modes_left_list).shape, np.array(X_list).shape)
     fraction_of_modes_left_list = np.array(n_modes_left_list) / n_modes_left_list[0]
     plt.plot(fraction_of_modes_left_list, np.zeros(len(X_list)) + 3, "--", label="Gaussian fixed point")
 
-    plt.ylim(0, 5)
     plt.ylabel("Normalized fourth moment")
     plt.xlabel("Fraction of modes left")
     plt.legend()
     plt.show()
+
+def plot_eigenvectors(u_list, n):
+    """
+
+    Parameters:
+        u_list - list of eigenvectors during coarse-graining
+        n - number of largest eigenvalues to plot
+    """
+    for i in range(n):
+
+        fig, ax = plt.subplots(1, 2)    
+
+        # Change indecis i (i think)
+        ax[0].imshow(u_list[0][:, i].reshape(u_list[0][:, i].size, 1) @ u_list[0][i].reshape(u_list[0][i].size, 1).T)
+        ax[0].set_title(f"Before coarse-graining")
+
+        ax[1].imshow(u_list[-1][i].reshape(u_list[0][i].size, 1) @ u_list[-1][i].reshape(u_list[0][i].size, 1).T)
+        ax[1].set_title(f"After {len(u_list)} steps of coarse-grainig")
     
+        plt.show()
+
 if __name__ == "__main__":
 
     # Read input 
     X = read_input()
     X = check_dataset_shape(X.T)
+    print(f"Shape of input data = {X.shape}")
 
     # Perform RG in momentum space 
-    X_list, u_list, s_list, n_modes_left_list = momentum_space_rg(X, cut_off=0.7)
+    cut_off = 0.5
+    step_ratio = 0.10
+    X_list, u_list, s_list, n_modes_left_list = momentum_space_rg(X, step_ratio, cut_off, pca_method="covariance")
 
     # Plot eigenspectrum
     plot_eigenspectrum(s_list, n_modes_left_list)
 
     # Plot normalized fourth moment
     plot_normalized_fourth_moment(X_list, n_modes_left_list)
+
+    # Show n largest eigenvectors 
+    n = len(u_list[-1])
+    print(n)
+    plot_eigenvectors(u_list, n)
--- real_space.py
+++ real_space.py
@@ -1,15 +1,31 @@
-import enum
 import matplotlib.pyplot as plt 
 import numpy as np
-import math
+import pandas as pd
 from data_cleaning import *
+from scipy.optimize import minimize
 
 def real_space_rg_iteration(X, correlation, test=False):
     """
+    Perform a single RG iteration. Given the dataset X and its correlation matrix we greedily pair the 
+    highest correlated pairs with each other. 
+
+    Parameters:
+        X - 2d np array containing the data
+        correlation - 2d np.array containing the correlation matrix of the data
+        test (optional) - if set to True it runs a simple test on a 2 by 2 lattice system. See output for more information.
+
+    Return:
+        X_coarse - the coarse grained variables. Size = [len(X)/2, len(X)/2]
+        pairings - list of indices that were paired. 
     """
-    # Not interested in self-correlation
-    np.fill_diagonal(correlation, 0)
+    # Initialize
     X_coarse = np.zeros((X.shape[0]//2, X.shape[1]))
+    pairings = []
+    list_of_original_indices = np.array([np.arange(len(X)), np.arange(len(X))])
+
+    # Interested in absolute correlation
+    correlation = np.abs(correlation)
+    np.fill_diagonal(correlation, 0)
 
     if test == True:
         correlation = np.array([[0, 0, 7, 0], [0, 0, 0, 3], [7, 0, 0, 0], [0, 3, 0, 0]])
@@ -19,22 +35,24 @@ def real_space_rg_iteration(X, correlation, test=False):
         print(f"The correlation matrix is given by:\n{correlation}")
         print(f"Original dataset X:\n{X}")
 
-    # Find best pairings
-    pairings = []
-    list_of_original_indices = np.array([np.arange(len(X)), np.arange(len(X))])
 
     for i in range(len(correlation) // 2):
-        # Find highest correlated pair
+        # Find highest correlated pair from correlation matrix
         max_idx = correlation.argmax()
         max_i, max_j = max_idx // len(correlation), max_idx % len(correlation)
 
-        # Remove the corresponding row and column
+        if max_i == max_j:
+            print('found diagonal element')
+            print(correlation)
+
+        # Remove the corresponding row and column from correlation matrix
         correlation = np.delete(correlation, [max_i, max_j], axis=0)
         correlation = np.delete(correlation, [max_i, max_j], axis=1)
 
         # Save found pairing
         max_i_original = list_of_original_indices[0][max_i]
         max_j_original = list_of_original_indices[1][max_j]
+
         pairings.append([max_i_original, max_j_original])
         list_of_original_indices = np.delete(list_of_original_indices, [max_i, max_j, max_i + len(list_of_original_indices[0]), max_j + len(list_of_original_indices[0])])
 
@@ -43,7 +61,6 @@ def real_space_rg_iteration(X, correlation, test=False):
             list_of_original_indices = list_of_original_indices.reshape(-1, len(correlation))
         elif len(list_of_original_indices) == 1:
             pairings.append(list_of_original_indices[0])
-
 
         # Merge pair in dataset also
         X_coarse[i] = (X[max_i_original] + X[max_j_original]) / 2
@@ -65,9 +82,55 @@ def compute_couplings(X):
     Return:
         couplings - list of couplings
     """
-    #print(X.shape)
-    couplings = []
+    # Create pd for complete model
+    f1 = np.array([[1,1],[1,-1]])
+    f2 = np.kron(f1,f1)
+    f4 = np.kron(f2,f2)
+    f8 = np.kron(f4,f4)
+    n = 8
+
+    # Data average 
+    X_df = pd.DataFrame(X.T)
+    print(X_df.shape)
+    n_rows, n_cols = X_df.shape[0], X_df.shape[1]
+    cols = list(range(n_cols))
+    X_df = X_df.groupby(cols)[0].count()
+
+    X_unique = X_df.index.tolist()
+    X_counts = X_df.values.tolist()
+    states_strings = [''.join([str(s) for s in state]) for state in X_unique]
+    data_int = [int(s,2) for s in states_strings]
+    g_data = np.zeros(2**n)
+    g_data[data_int] = np.array(X_counts) / sum(X_counts)
+    print(data_int, X_counts)
+
+    # Initial parameters
+    g = np.random.rand(2**n)
+    
+    epsilon = 0.1
+
+    N = 1000
+    for i in range(1):
+        _, dkl_grad = dkl(g, f8, g_data)
+        g += dkl_grad
     return couplings
+
+def dkl(g, f, g_data):
+    # Complete model probability distribution
+    z = np.sum(np.exp(np.dot(f, g)))
+    pd_complete_model = np.exp(np.dot(f, g)) / z
+
+    # Data probability distribution
+    z_data = np.sum(np.exp(np.dot(f, g_data)))
+    pd_data = np.exp(np.dot(f, g)) / z_data
+
+    l = np.log(pd_complete_model / pd_data)
+    l[np.isinf(l)] = 0 
+    l[np.isnan(l)] = 0
+    dkl = np.dot(pd_complete_model, l)
+    dkl_grad = np.dot(f.T, pd_data-pd_complete_model)
+    #print(dkl_grad)
+    return dkl, dkl_grad
 
 def add_to_clusters(clusters, pairings):
     """
@@ -87,7 +150,7 @@ def add_to_clusters(clusters, pairings):
 
     # Loop over pairings found and create new clusters
     new_clusters = []
-    for i, pair in enumerate(pairings):
+    for _, pair in enumerate(pairings):
         if len(pair) == 1: # This variable was not paired
             new_cluster = pair
         elif len(pair) == 2:
@@ -103,19 +166,20 @@ def real_space_rg(X, steps, test=False):
     """
     """
     X_list = []
+    clusters_list = []
     clusters = []
     X_coarse = X
-    couplings = []
+    coupling_parameters = []
 
     # Perform RG iterations
     for i in range(steps):
         # Cannot coarse any further
         if len(X_coarse) == 1:
             print("Finished coarse-graining!")
-            return X_list, clusters
+            return np.array(X_list), np.array(clusters_list), np.array(coupling_parameters)
 
         # Compute couplings
-        couplings.append(compute_couplings(X_coarse))
+        # coupling_parameters.append(compute_couplings(X_coarse))
 
         # Compute correlation
         correlation = np.corrcoef(X_coarse)
@@ -128,13 +192,53 @@ def real_space_rg(X, steps, test=False):
 
         # Add pairing to clusters
         clusters = add_to_clusters(clusters, pairings)
+        clusters_list.append(np.array(clusters))
 
-    return X_list, clusters 
+    return np.array(X_list), np.array(clusters_list), np.array(coupling_parameters)
+
+def show_clusters_by_imshow(clusters):
+    """
+    """
+
+    original_size = len(clusters[0][0]) * len(clusters[0][:, 0])
+    grid = np.zeros((original_size, original_size))
+    for c in clusters[:-1]:
+
+        colors = np.arange(1, 1 + len(c[:,0])) / len(c[:,0]) * 10
+        for i, color in enumerate(colors):
+            for j in c[i]:
+                grid[j, c[i]] = color
+
+        print(f"Clusters = {c}")
+        plt.imshow(grid)
+        plt.show()
+
+def plot_normalized_activity(X_list):
+    """
+    """
+    n_vars = X_list[0].shape[0] # Variables in dataset before coarse-graining
+    for X in X_list:
+        unique_values, inverse = np.unique(X, return_inverse=True)
+        bins = np.bincount(inverse)
+
+        plt.plot(unique_values, bins / sum(bins), "o--", label=f"K = {n_vars // X.shape[0]}")
+    
+    plt.ylabel("probability")
+    plt.xlabel("normalized activity")
+    plt.title("Probability distribution of the normalized activity")
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
     # Read input 
     X = read_input()
     X = check_dataset_shape(X.T)
+    print(f"Shape of input data = {X.shape}")
 
     steps = 5
-    X_coarse, clusters = real_space_rg(X, steps, test=False)
+    test = False    
+    X_coarse, clusters, coupling_parameters = real_space_rg(X, steps, test=test)
+
+    show_clusters_by_imshow(clusters)
+
+    plot_normalized_activity(X_coarse)
